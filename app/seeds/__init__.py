126b1dc886f2fb73a69137a3a442abb75d0eb280
bradsimpson213 <bradsimpson@icloud.com>
2023-09-26 12:25:00 -0400

    fixed heroku deploy

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,7 +2,10 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .positions import seed_positions, undo_positions
 from .locations import seed_locations, undo_locations
+import os
 
+
+environment = os.getenv("FLASK_ENV")
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
@@ -11,6 +14,13 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    if environment == 'production':
+        # Before seeding in production, you want to run the seed undo 
+        # command, which will  truncate all tables 
+        undo_users()
+        undo_locations()
+        undo_positions()
+
     seed_positions()
     seed_locations()
     seed_users()
